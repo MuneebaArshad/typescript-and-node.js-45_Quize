350cbc6fb48fb0fbb8aa2b1bf87055db548cfa39
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:27:42 +0500

    Add files via upload

---
+++ Quiz_11/main.js
@@ -0,0 +1,9 @@
+// Qno:11
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+// Store the names friends in an array
+var names = ["Ashar", "Arshad", "Ayan", "Subhan"];
+// Print each person name 
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
