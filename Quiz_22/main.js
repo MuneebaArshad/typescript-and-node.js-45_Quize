10d5beda010a2a6729132f4c812325822c157c95
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:40:35 +0500

    Add files via upload

---
+++ Quiz_22/main.js
@@ -0,0 +1,8 @@
+// Qno:22
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+// fruit name store in array
+var fruit = ["Appla", "Mango", "Banana", "Orange"];
+// add Intentional error 
+console.log(fruit, { 3:  });
+// Correct the error  
+console.log(fruit[3]);
