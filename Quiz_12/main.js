0f35bcc0998b0c1e123bea3b7e825e4b74888c39
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:28:22 +0500

    Add files via upload

---
+++ Quiz_12/main.js
@@ -0,0 +1,12 @@
+// Qno:12
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+// Store the names 
+var names = ["Arshad", "Hina", "Muneeba", "Subhan", "Ayan"];
+// Store the message  
+var message = "Do you like to learn typescript?";
+// Print message to each person.
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
+console.log(names[4] + " " + message);
---
+++ Quiz_12/main.ts
@@ -0,0 +1,16 @@
+// Qno:12
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+// Store the names 
+let names:string [] = ["Arshad","Hina","Muneeba","Subhan","Ayan"];
+
+// Store the message  
+let message:string ="Do you like to learn typescript?";
+
+// Print message to each person.
+console.log(names[0] +" "+ message);
+console.log(names[1] +" "+ message);
+console.log(names[2] +" "+ message);
+console.log(names[3] +" "+ message);
+console.log(names[4] +" "+ message);
+
