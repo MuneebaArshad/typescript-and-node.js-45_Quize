42e2ee2f3107408379e5a70bff7ea2773a70b400
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:24:31 +0500

    Add files via upload

---
+++ Quiz_7/main.js
@@ -0,0 +1,31 @@
+// Question:7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+//  addition operation
+var byAddition = 5 + 3;
+console.log("Addition Result: " + byAddition);
+// subtraction operation
+var bySubtraction = 10 - 2;
+console.log("Subtraction Result: " + bySubtraction);
+//  multiplication operation
+var byMultiplication = 4 * 2;
+console.log("Multiplication Result: " + byMultiplication);
+//  division operation
+var byDivision = 16 / 2;
+console.log("Division Result: " + byDivision);
+//  Another Method 
+// addition operation
+var z = 5;
+var y = 3;
+console.log(z + y);
+// subtraction operation
+var a = 10;
+var b = 2;
+console.log(a - b);
+//   multiplication operation
+var c = 4;
+var d = 2;
+console.log(c * d);
+//  division operation
+var e = 16;
+var f = 2;
+console.log(e / f);
---
+++ Quiz_7/main.ts
@@ -0,0 +1,40 @@
+// Question:7
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+
+//  addition operation
+ const byAddition = 5+3;
+console.log("Addition Result: "+byAddition);
+
+// subtraction operation
+const bySubtraction = 10-2;
+console.log("Subtraction Result: " +bySubtraction);
+
+//  multiplication operation
+ const byMultiplication = 4*2;
+ console.log("Multiplication Result: " +byMultiplication);
+
+//  division operation
+const byDivision= 16/2;
+console.log("Division Result: "+byDivision);
+
+//  Another Method 
+// addition operation
+let  z:number = 5;
+let  y:number = 3;
+console.log(z + y);
+
+// subtraction operation
+let  a:number = 10; 
+let  b:number = 2;
+console.log(a - b);
+
+//   multiplication operation
+let  c:number = 4; 
+let  d:number = 2;
+console.log(c * d);
+ 
+//  division operation
+let  e:number = 16;
+let  f:number = 2;
+console.log(e / f);
