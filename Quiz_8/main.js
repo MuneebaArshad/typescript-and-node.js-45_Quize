bfd6b17152a3883005edc9095bdd0b98eaaa32b2
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:24:56 +0500

    Add files via upload

---
+++ Quiz_8/main.js
@@ -0,0 +1,12 @@
+// Question:8
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+//  Addition
+console.log(5 + 3);
+// subtraction
+console.log(10 - 2);
+// multiplication
+console.log(4 * 2);
+// division
+console.log(16 / 2);
