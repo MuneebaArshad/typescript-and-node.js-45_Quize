3e0fe35ce3818bc6ef2dbfbc4f053f084d5fd22a
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 01:01:44 +0500

    Add files via upload

---
+++ Quiz_36/main.js
@@ -0,0 +1,7 @@
+// Qno:36
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function makeshirt(size, text) {
+    console.log(" you order a ".concat(size, " shirt that says ").concat(text));
+}
+makeshirt('small', '"i love shirt"');
+makeshirt('Large', '"i love Large shirt"');
