cb4d32974c6e6ea7790b0db7a90cbca847dc5609
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:22:33 +0500

    Add files via upload

---
+++ Quiz_4/main.ts
@@ -0,0 +1,8 @@
+// Qno:4
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+// Store the author
+let person_name:string = "Ralpha Waldo Emerson";
+// Print the quote and its author
+console.log(person_name+ " once said," + " Life is a journey,not a destination.");
