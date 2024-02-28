449c74e1b90cc917f7dab7b115d955f4214c3399
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:47:34 +0500

    Add files via upload

---
+++ Quiz_31/main.ts
@@ -0,0 +1,34 @@
+// // Qno:31
+//  /*No Users: Add an if test to Exercise 30 to make sure the list of users is not empty.
+// • If the list is empty, print the message We need to find some users!
+
+// • Remove all of the usernames from your array, and make sure the correct message is printed.*/
+
+
+let userNames: string[] = ['admin', 'eric', 'Ali', 'Ayan', 'Subhan'];
+
+if (userNames.length === 0){
+    console.log ("we need to find some work")
+}else{
+    for(let user of userNames){
+        if (user === "admin"){
+            console.log("Hello admin, Would you like to see a status report?")
+        }else{
+            console.log(`Hello ${user}, thank you for login again`)
+        }
+    }
+}
+
+
+userNames = []
+if (userNames.length ===0){
+    console.log("we need to find some users!")
+}
+
+
+
+
+
+
+
+ 
