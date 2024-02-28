6e8551c96032dbb8d74cde987639b4fafa3ca12d
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 01:09:49 +0500

    Add files via upload

---
+++ Quiz_9/main.js
@@ -0,0 +1,10 @@
+// Qno:9
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+// 1 Method
+var fvt = 29;
+var message = "it's my birth date.";
+console.log("".concat(fvt, " ").concat(message));
+// 2 Method 
+var favrt_number = 29;
+var msg = "My favrt number is ".concat(favrt_number, " Bcz Its my Birth date.");
+console.log(msg);
---
+++ Quiz_9/main.ts
@@ -0,0 +1,12 @@
+// Qno:9
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+// 1 Method
+let fvt:number = 29;
+let message:string = "it's my birth date.";
+console.log(`${fvt} ${message}`);
+
+// 2 Method 
+let favrt_number = 29;
+let msg = `My favrt number is ${favrt_number} Bcz Its my Birth date.`;
+console.log(msg);
