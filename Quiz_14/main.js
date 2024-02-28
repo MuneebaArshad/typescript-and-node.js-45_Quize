25f5894fd45754dd39767e40652dea25174e3ec1
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:33:27 +0500

    Add files via upload

---
+++ Quiz_14/main.js
@@ -0,0 +1,8 @@
+// Qno:14
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner
+// Store a guest list
+var guest = ["Ayan", "Ahsan", "Subhan"];
+// Print an invitation message 
+guest.forEach(function (guestName) {
+    console.log("Dear ".concat(guestName, ",bro you are invited to the dinner"));
+});
---
+++ Quiz_14/main.ts
@@ -0,0 +1,13 @@
+// Qno:14
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner
+
+
+ // Store a guest list
+let guest:string [] = ["Ayan","Ahsan","Subhan"];
+
+// Print an invitation message 
+guest.forEach((guestName) => 
+{
+ console.log(`Dear ${guestName},bro you are invited to the dinner`)
+});
+
