7df34522d473179f0f89e1a520e5c14e3a79a801
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:26:44 +0500

    Add files via upload

---
+++ Quiz_10/main.js
@@ -0,0 +1,13 @@
+// Question:10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// Name: Muneeba Arshad Ali
+// Date: 11-Feb-2024
+// store my favourite number.
+var favrt_number = 29;
+// Reveling my favourite number in a message formate.
+var msg = "My favrt number is ".concat(favrt_number, " Bcz Its my Birth date.");
+console.log(msg);
+// Store the  author name.
+var person_name = "Ralpha Waldo Emerson";
+// Printing the quote and its author
+console.log(person_name + " once said," + " Life is a journey,not a destination.");
---
+++ Quiz_10/main.ts
@@ -0,0 +1,18 @@
+// Question:10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// Name: Muneeba Arshad Ali
+// Date: 11-Feb-2024
+
+// store my favourite number.
+let favrt_number = 29;
+
+// Reveling my favourite number in a message formate.
+let msg = `My favrt number is ${favrt_number} Bcz Its my Birth date.`;
+console.log(msg);
+
+
+// Store the  author name.
+let person_name:string = "Ralpha Waldo Emerson";
+// Printing the quote and its author
+console.log(person_name+ " once said," + " Life is a journey,not a destination.");
