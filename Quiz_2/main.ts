e75066c9bb267110785567e224c75d726d33d2d1
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 01:07:45 +0500

    Add files via upload

---
+++ Quiz_2/main.ts
@@ -0,0 +1,20 @@
+// Question 2
+//  2 .Personal Message: Store a person’s name in a variable, and print a message to that person. 
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// console.log("\tQuestion 2:\n");
+
+
+// 1 Method
+// Store a person's name in a variable
+let fName = "Ayan,";
+
+// Print a message
+console.log("Hello", fName , "Would you like to learn TypeScript today?");
+
+ 
+// 2 Method
+// Store a person's name in a variable
+ let myname:string ="Ayan,"
+ 
+ // Print a  message
+console.log("Hello "+ myname +  " would you like to learn some Python today?");
