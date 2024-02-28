80248194833347a263b1d5b2e7063020cdd3a9f2
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:37:47 +0500

    Add files via upload

---
+++ Quiz_18/main.ts
@@ -0,0 +1,51 @@
+// Qno:18
+
+/*Seeing the World: Think of at least five places in the world you’d like to visit.
+• Store the locations in a array. Make sure the array is not in alphabetical order.
+
+• Print your array in its original order.
+
+• Print your array in alphabetical order without modifying the actual list.
+
+• Show that your array is still in its original order by printing it.
+
+• Print your array in reverse alphabetical order without changing the order of the original list.
+
+• Show that your array is still in its original order by printing it again.
+
+• Reverse the order of your list. Print the array to show that its order has changed.
+
+• Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+• Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+• Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.*/
+
+// store places  
+let place:string [] = ['Pakistan','Dubai','China','Maldives','New Zealand'];
+
+// Print your array in its original order.
+console.log("Orignal order:",place);
+
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+console.log("Reverse alphabatical order:",[...place].sort().reverse());
+
+// • Show that your array is still in its original order by printing it.
+console.log("Orignal order reverse sorting:",place);
+
+// • Reverse the order of your list. Print the array to show that its order has changed.
+place.reverse();
+console.log("Orignal order reverse sorting:",place);
+
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+place.reverse();
+console.log("Orignal order reverse sorting:",place);
+
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+place.sort();
+console.log("Sorted in alphabetical order:",place);
+
+ // Print array in reverse alphabetic order
+ place.sort((a, b) => b.localeCompare(a));
+ console.log("Reverse alphabetical order",place)
+ 
