91d11dac9b106ba6a207f3c3c4b527c291cb0137
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:39:15 +0500

    Add files via upload

---
+++ Quiz_20/main.js
@@ -0,0 +1,11 @@
+// Qno:20
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items
+// Create an array of countries
+var countries = ["Pakistan", "NewYourk", "Japan", "China", "Hong Kong"];
+console.log("List of Countries:");
+// the array using a for...of loop
+for (var _i = 0, countries_1 = countries; _i < countries_1.length; _i++) {
+    var country = countries_1[_i];
+    //  // Print each country in the array
+    console.log(country);
+}
---
+++ Quiz_20/main.ts
@@ -0,0 +1,16 @@
+// Qno:20
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items
+
+// Create an array of countries
+let countries:string[] = ["Pakistan","NewYourk","Japan","China","Hong Kong"];
+
+console.log("List of Countries:");
+ 
+// the array using a for...of loop
+for (let country of countries)
+
+//  // Print each country in the array
+console.log(country);
+
+
+
