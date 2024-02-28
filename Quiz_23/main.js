0d44dc10ecd67d1a509bf75571c0ca483ff28e23
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:41:23 +0500

    Add files via upload

---
+++ Quiz_23/main.js
@@ -0,0 +1,53 @@
+// Qno:20
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+var car = "subaru";
+// Task 1 true
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Task 2 true
+console.log("Is car != 'honda'? I predict True.");
+console.log(car != 'honda');
+// Test 3 true
+console.log("Is car.length > 0? I predict True.");
+console.log(car.length > 0);
+// Test 4 true
+console.log("Is car.length == 6 ? I predict True.");
+console.log(car.length == 6);
+// Test 5 true
+console.log("car == subaru? I predict True.");
+if (car == "subaru") {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+// Test 6 false
+console.log("car == honda? I predict false.");
+if (car == "honda") {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+// Test  7 false
+console.log("Is car.length == 7? I predict false.");
+console.log(car.length == 7);
+// Test 8 false
+console.log("Is car.length < 5? I predict False.");
+console.log(car.length < 5);
+// Test 9  false
+console.log("Is car !== 'subaru'? I predict true.");
+console.log(car !== 'subaru');
+// Test 10 false
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'Subaru');
