c93acaf911b69b6c1f20699300b8c9c7e0cf6676
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:23:42 +0500

    Add files via upload

---
+++ Quiz_5/main.js
@@ -0,0 +1,7 @@
+//  Qno:5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// Store the quote and author_name
+var author_name = "Ralpha Waldo Emerson";
+var Quote = "Life is a journey,not a destination";
+// Print the quote and  author_name
+console.log("".concat(author_name, " once said, \"").concat(Quote, "\""));
---
+++ Quiz_5/main.ts
@@ -0,0 +1,8 @@
+//  Qno:5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+ 
+// Store the quote and author_name
+let author_name:string = "Ralpha Waldo Emerson";
+let Quote:string = "Life is a journey,not a destination";
+// Print the quote and  author_name
+console.log(`${author_name} once said, "${Quote}"`);
