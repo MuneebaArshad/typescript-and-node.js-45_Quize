07a9238bcc79371e824ad6a77fb4eb524f03002b
Muneeba Arshad <161239150+MuneebaArshad@users.noreply.github.com>
2024-02-29 00:22:05 +0500

    Add files via upload

---
+++ Quiz_3/main.ts
@@ -0,0 +1,15 @@
+// Qno:3
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+// Store a person's name in a variable
+let personName:string = "Muneeba arshad ali";
+
+// Print name in lowercase
+console.log(personName.toLowerCase());
+
+//  Print name in uppercase
+console.log(personName.toUpperCase());
+
+ //  Print name in titlecase
+console.log(personName.replace(/\b\w/g,c=> c.toUpperCase()));
+
